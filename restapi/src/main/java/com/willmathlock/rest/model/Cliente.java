225bb9e8a167bf97f788d3417d3f9f71877cfca0
William <willmathlock>
2022-09-23 18:22:19 -0300

    removi o lombok, usando get/setter padrao

--- restapi/src/main/java/com/willmathlock/rest/model/Cliente.java
+++ restapi/src/main/java/com/willmathlock/rest/model/Cliente.java
@@ -1,15 +1,15 @@
 package com.willmathlock.rest.model;
 
 
+import java.util.Objects;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
-import lombok.Data;
 
-@Data
 @Entity
 public class Cliente {
 
@@ -17,8 +17,41 @@ public class Cliente {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	
-	@Column()
+	@Column(nullable = false)
 	private String nome;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Cliente other = (Cliente) obj;
+		return Objects.equals(id, other.id);
+	}
 
 	
 }
